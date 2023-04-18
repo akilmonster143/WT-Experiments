db2f3875db5cb289d12da2b89406c6028cb47a15
Akil paidi <89410678+akilmonster143@users.noreply.github.com>
2023-04-18 14:28:00 +0530

    Delete Experiment-2 directory

--- Experiment-2/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$username = "Akil";
-$password = "Akilamu@321";
-$database = "login_page";
-
-$conn = mysqli_connect($host, $username, $password);
-?>
--- Experiment-2/connection1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "localhost";
-$username = "Akil";
-$password = "Akilamu@321";
-$database = "mydb";
-?>
